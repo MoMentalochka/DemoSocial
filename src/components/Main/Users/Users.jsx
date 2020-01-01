bdbe3f7f611f1eb39a9f00f7cc00d970591fc6b8
MoMentalochka <56727108+MoMentalochka@users.noreply.github.com>
2020-01-01 20:38:00 +0300

    Part 53-54 SideEffect ClassComponent

--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -4,26 +4,27 @@ import User from './User/User';
 import { setUsersAC } from '../../../redux/UsersReducer'
 import * as axios from 'axios'
 import UserPhoto from '../../../Img/User.jpg'
-const Users = (props) => {
-    let Users =
-        props.state.usersPage.usersData.map(u => <User key={u.id} name={u.name} second__name={u.second__name} id={u.id} avatar={u.photos.small != null ? u.photos : UserPhoto} followed={u.followed} dispatch={props.dispatch} />)
 
-
-
-    let GetUsers = () => {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            let data = response.data.items
-            props.dispatch(setUsersAC(data))
+class Users extends React.Component {
+    
+    constructor(props){
+        super(props);
+        alert('New Users')
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+            this.props.dispatch(setUsersAC(response.data.items))
         });
-
-
     }
-    return (
-        <div className={styles.news}>
+    render(){
+        let Users =
+        this.props.state.usersPage.usersData.map(u => <User key={u.id} name={u.name} second__name={u.second__name} id={u.id} avatar={u.photos.small != null ? u.photos : UserPhoto} followed={u.followed} dispatch={this.props.dispatch} />)
+        return (
+            <div className={styles.news}>
             {Users}
-            <div > <input type="button" value="Show more" onClick={GetUsers} /> </div>
-        </div>
-    )
+                <div > <input type="button" value="Show more"  /> </div>
+            </div>
+        )
+    }
 }
 
 export default Users;

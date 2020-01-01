f332961460be5b199eae64af436e7e561f2556ee
MoMentalochka <56727108+MoMentalochka@users.noreply.github.com>
2020-01-01 12:29:34 +0300

    Part 50-51 Axios Get UsersData!

--- src/components/Main/Users/User/User.jsx
+++ src/components/Main/Users/User/User.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import {followAC,unfollowAC} from "../../../../redux/UsersReducer"
+import { followAC, unfollowAC } from "../../../../redux/UsersReducer"
 import styles from './User.module.css'
 let User = (props) => {
-    
-    let follow = () =>{
+
+    let follow = () => {
         props.dispatch(followAC(props.id));
+        alert('Теперь ' + props.name + " " + props.second__name + ' ваш друг')
     }
-    let unfollow = () =>{
-        props.dispatch(unfollowAC(props.id)); 
-       
+    let unfollow = () => {
+        props.dispatch(unfollowAC(props.id));
+        alert('Вы отписались от ' + props.name + " " + props.second__name);
     }
-let t = (props.followed !== true) ? 'Follow' : 'Unfollow';
-let a = (props.followed === true) ? unfollow : follow;
-let bstyle = (props.followed !== true) ? styles.follow : styles.unfollow;
-    return(
-    <div className={styles.user}>
-        <div className={styles.avatar}><img src={props.avatar} alt=""/></div>
-        <div className={styles.name}>{props.name} <br/> {props.second__name}</div>
-        <button onClick={a} className={bstyle}> {t}</button>
-    </div>
+    let t = (props.followed !== true) ? 'Follow' : 'Unfollow';
+    let a = (props.followed === true) ? unfollow : follow;
+    let bstyle = (props.followed !== true) ? styles.follow : styles.unfollow;
+    return (
+        <div className={styles.user}>
+            <div className={styles.avatar}><img src={props.avatar} alt="" /></div>
+            <div className={styles.name}>{props.name} <br /> {props.second__name}</div>
+            <button onClick={a} className={bstyle}> {t}</button>
+        </div>
     )
 }
 export default User;
--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import styles from './Users.module.css';
 import User from './User/User';
+import { setUsersAC } from '../../../redux/UsersReducer'
+import * as axios from 'axios'
+import UserPhoto from '../../../Img/User.jpg'
+const Users = (props) => {
+    let Users =
+        props.state.usersPage.usersData.map(u => <User key={u.id} name={u.name} second__name={u.second__name} id={u.id} avatar={u.photos.small != null ? u.photos : UserPhoto} followed={u.followed} dispatch={props.dispatch} />)
 
-const Users = (props) =>{
-let Users = 
-props.state.usersPage.usersData.map( u => <User key={u.id} name={u.name} second__name={u.second__name} id={u.id} avatar={u.avatar} followed ={u.followed} dispatch ={props.dispatch}/>)
-    return <div className={styles.news}>
-    {Users}
-    </div>  
+
+
+    let GetUsers = () => {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            let data = response.data.items
+            props.dispatch(setUsersAC(data))
+        });
+
+
+    }
+    return (
+        <div className={styles.news}>
+            {Users}
+            <div > <input type="button" value="Show more" onClick={GetUsers} /> </div>
+        </div>
+    )
 }
+
 export default Users;

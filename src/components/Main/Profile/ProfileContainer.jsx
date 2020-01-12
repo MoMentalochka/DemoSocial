ab275babefaf4220ca87d24ec85da9224a34b7c2
MoMentalochka <56727108+MoMentalochka@users.noreply.github.com>
2020-01-13 01:56:31 +0300

    fix

--- src/components/Main/Profile/ProfileContainer.jsx
+++ src/components/Main/Profile/ProfileContainer.jsx
@@ -12,7 +12,6 @@ class ProfileContainer extends React.Component {
 
     componentDidMount() {
         let userId = this.props.match.url
-        alert(userId)
         axios.get(`https://social-network.samuraijs.com/api/1.0${userId}`)
             .then(response => {
                 this.props.setUserProfile(response.data)

9f2ae0475f6e496bc72ebaed0042e9044038efa4
MoMentalochka <56727108+MoMentalochka@users.noreply.github.com>
2020-01-13 00:46:08 +0300

    Updates

---
+++ precache-manifest.e2d4b44d4f54a3cb52253d2c2a21af61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b285776798759ef2234db15dec72e4c",
+    "url": "/DemoSocial/index.html"
+  },
+  {
+    "revision": "69ef268bea84aeda5cb1",
+    "url": "/DemoSocial/static/css/main.fecc796b.chunk.css"
+  },
+  {
+    "revision": "aa49594b317857b580fc",
+    "url": "/DemoSocial/static/js/2.8e7b1202.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/DemoSocial/static/js/2.8e7b1202.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69ef268bea84aeda5cb1",
+    "url": "/DemoSocial/static/js/main.0de36868.chunk.js"
+  },
+  {
+    "revision": "112a87d55a05c207d6f8",
+    "url": "/DemoSocial/static/js/runtime-main.1e9186bc.js"
+  },
+  {
+    "revision": "a1107919e7727d87fe32c89a886a431c",
+    "url": "/DemoSocial/static/media/User.a1107919.jpg"
+  },
+  {
+    "revision": "1d15156b8f3828f398699157696827b6",
+    "url": "/DemoSocial/static/media/preloader.1d15156b.svg"
+  }
+]);
